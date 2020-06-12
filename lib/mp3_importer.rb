5d71bd6fd2a948ab73139e5ef241a42a1336db42
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-12 01:26:03 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,21 +1,30 @@
 class MP3Importer
-  attr_accessor :path
-
-  def initialize(path)
-    @path = path
-  end
-
-  def files
-    files = []
-    Dir.new(self.path).each do |file|
-      files << file if file.length > 4
+attr_accessor :path
+    
+    @@all = []
+    def initialize(file_path)
+        @path =file_path
+        save
     end
-    files
-  end
 
-  def import
-    self.files.each do |filename|
-      Song.new_by_filename(filename)
+    def files
+        files = Dir.entries(@path)
+        mp3 = files.map{|file| file if file.match(/.mp3/)}.select{|k| k}
+        mp3
     end
-  end
+
+    def import
+        new_songs= files.map{|file| file.gsub('.mp3', '')}
+        new_songs.each{|song| Song.new_by_filename(song)}
+    end
+
+
+    def save 
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
