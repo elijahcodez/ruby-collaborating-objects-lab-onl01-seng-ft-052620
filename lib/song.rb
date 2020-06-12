f3ccce80829fbfbcc499c0a957856d2729946a11
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-12 01:26:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,32 @@
 class Song
+ attr_accessor :name, :artist
 
-  attr_accessor :name, :artist
+ @@all =[]
+ 
+def initialize(name)
+    @name =name
+    save
+end
 
-  def initialize(name)
-    @name = name
-  end
-
-  def self.new_by_filename(file_name)
-    song = file_name.split(" - ")[1]
-    artist = file_name.split(" - ")[0]
+def self.new_by_filename(file)
+    file = file.split(' - ').map{|each|each.strip}
+    song = file[1]
+    artist = file[0]
     new_song = self.new(song)
-    new_song.artist_name = artist
+    new_song.artist_name= artist
     new_song
-  end
+end
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
+def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by_name(artist_name)
     artist.add_song(self)
-  end
+end
+def save 
+    @@all << self
+end
+
+def self.all
+    @@all
+end
 
 end
